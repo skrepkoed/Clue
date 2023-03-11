f1875b38fe1af502d0d9bba212f024b3df4985d3
skrepkoed <artempecenkin240@gmail.com>
2023-03-11 13:42:51 +0500

    Initial clue game project

---
+++ android/src/com/clue/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.clue.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.clue.game.ClueGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ClueGame(), config);
+	}
+}
